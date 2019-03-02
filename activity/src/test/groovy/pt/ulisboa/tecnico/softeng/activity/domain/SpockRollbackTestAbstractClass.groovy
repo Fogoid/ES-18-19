0d7abb45b05e872e96a52c300182daab3f8711f7
Diogo Fernandes <diogo.barradas.fernandes@gmail.com>
2019-03-02 15:46:37 +0000

    RollbackTestAbstractClass migrated to groovy

---
+++ activity/src/test/groovy/pt/ulisboa/tecnico/softeng/activity/domain/SpockRollbackTestAbstractClass.groovy
@@ -0,0 +1,29 @@
+package pt.ulisboa.tecnico.softeng.activity.domain
+
+import pt.ist.fenixframework.FenixFramework
+import pt.ist.fenixframework.core.WriteOnReadError
+
+import javax.transaction.NotSupportedException
+import javax.transaction.SystemException
+
+abstract class SpockRollbackTestAbstractClass {
+
+    def setUp() throws Exception {
+        try {
+            FenixFramework.getTransactionManager().begin(false)
+            populate4Test()
+        } catch (WriteOnReadError | NotSupportedException | SystemException el) {
+            el.printStackTrace()
+        }
+    }
+
+    def tearDown() {
+        try{
+            FenixFramework.getTransactionManager().rollback()
+        } catch (IllegalStateException | SecurityException | SystemException e){
+            e.printStackTrace()
+        }
+    }
+
+    def abstract populate4Test()
+}
