a4e085218e71d1ec0d76ba045db38e1dc25dabb1
Fogoid <diogo.barradas.fernandes@gmail.com>
2019-03-07 13:17:18 +0000

    [#34] Rename SpockOperationRevertMethodTest's populate function name
    Change name from "populate for test" to populate4Test
    
    Fix: #34

--- bank/src/test/groovy/pt/ulisboa/tecnico/softeng/bank/domain/SpockOperationRevertMethodTest.groovy
+++ bank/src/test/groovy/pt/ulisboa/tecnico/softeng/bank/domain/SpockOperationRevertMethodTest.groovy
@@ -6,36 +6,36 @@ class SpockOperationRevertMethodTest extends SpockRollbackTestAbstractClass{
     private Account account
 
     def "reversing a deposit"() {
-        given:
+        given: "a new deposit on an account"
         def reference = account.deposit(100).getReference()
         def operation = bank.getOperation(reference)
 
-        when:
+        when: "the operation is reversed"
         def newReference = operation.revert()
 
-        then:
+        then: "the values must be those without the given deposit"
         assert account.getBalance() == 0
         assert bank.getOperation(newReference)
         assert bank.getOperation(reference)
     }
 
     def "reversing a withdraw"() {
-        given:
+        given: "a new deposit followed by a withdraw"
         this.account.deposit(1000)
         def reference = account.withdraw(100).getReference()
         def operation = bank.getOperation(reference)
 
-        when:
+        when: "the operation is reversed"
         def newReference = operation.revert()
 
-        then:
+        then: "the values must be those without the given withdraw"
         assert account.getBalance() == 1000
         assert bank.getOperation(newReference)
         assert bank.getOperation(reference)
     }
 
     @Override
-    def "populate for test"() {
+    def populate4Test() {
         bank = new Bank("Money","BK01")
         def client = new Client(bank, "António")
         account = new Account(bank, client)
