60ae30b7fb861622fc9bbde5871c0c543346a2f3
Fogoid <diogo.barradas.fernandes@gmail.com>
2019-03-11 00:27:37 +0000

    [#47] Implement IRSGetItemTypeByNameTest in groovy
    Migrated completelly the referenced class from java into groovy using unroll and spock framework
    
    Close: #47

---
+++ tax/src/test/groovy/pt/ulisboa/tecnico/softeng/tax/domain/IRSGetItemTypeByNameSpockTest.groovy
@@ -0,0 +1,38 @@
+package pt.ulisboa.tecnico.softeng.tax.domain
+
+import spock.lang.Unroll
+
+
+def class IRSGetItemTypeByNameSpockTest extends SpockRollbackTestAbstractClass {
+	def FOOD = "FOOD"
+	def VALUE = 16
+
+	private IRS irs
+
+	@Override
+	def populate4Test() {
+		irs = IRS.getIRSInstance()
+		new ItemType(irs, FOOD, VALUE)
+	}
+
+	def "getting an item type by name success"() {
+		when: 'an item type is created successfully'
+		ItemType itemType = this.irs.getItemTypeByName(FOOD)
+
+		then: 'the following properties must be met'
+		itemType.getName() != null
+		itemType.getName() == FOOD
+	}
+
+	@Unroll('irs.getItemTypeByName: #name')
+	def 'check invalid names'() {
+		when: 'creating an item type with invalid name'
+		def itemType = irs.getItemTypeByName(name)
+
+		then:
+		itemType == null
+
+		where:
+		name << [null, '', 'CAR']
+	}
+}
