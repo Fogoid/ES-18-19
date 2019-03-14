913d0d22d5d2bd5c96d2df05b883f09974b02324
Antunes10 <andre.r.antunes@ist.utl.pt>
2019-03-14 03:56:36 +0000

    [#58] IRSCancelInvoiceMethodTest
    migrate IRSCancelInvoiceMethodTest to groovy
    
    close:#58

---
+++ tax/src/test/groovy/services/local/SpockIRSCancelInvoiceMethodTest.groovy
@@ -0,0 +1,74 @@
+package services.local
+
+import pt.ulisboa.tecnico.softeng.tax.domain.SpockRollbackTestAbstractClass
+import org.joda.time.LocalDate
+import pt.ulisboa.tecnico.softeng.tax.domain.Buyer;
+import pt.ulisboa.tecnico.softeng.tax.domain.IRS;
+import pt.ulisboa.tecnico.softeng.tax.domain.Invoice;
+import pt.ulisboa.tecnico.softeng.tax.domain.ItemType;
+import pt.ulisboa.tecnico.softeng.tax.domain.RollbackTestAbstractClass;
+import pt.ulisboa.tecnico.softeng.tax.domain.Seller;
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+import pt.ulisboa.tecnico.softeng.tax.services.local.TaxInterface;
+
+class SpockIRSCancelInvoiceMethodTest extends SpockRollbackTestAbstractClass{
+
+    def SELLER_NIF = "123456789"
+    def BUYER_NIF = "987654321"
+    def FOOD = "FOOD"
+    def VALUE = 16
+    LocalDate date = new LocalDate(2018, 02, 13)
+
+    private IRS irs
+    private String reference
+    Invoice invoice
+
+    def populate4Test() {
+        irs = IRS.getIRSInstance()
+        Seller seller = new Seller(irs, SELLER_NIF, "José Vendido", "Somewhere")
+        Buyer buyer = new Buyer(irs, BUYER_NIF, "Manuel Comprado", "Anywhere")
+        ItemType itemType = new ItemType(irs, FOOD, VALUE)
+        invoice = new Invoice(30.0, date, itemType, seller, buyer)
+        reference = invoice.getReference()
+    }
+
+    def 'success'() {
+
+        when:
+        TaxInterface.cancelInvoice(reference)
+
+        then:
+        invoice.isCancelled()
+    }
+
+
+    def 'nullReference'() {
+
+        when:
+        TaxInterface.cancelInvoice(null)
+
+        then:
+        thrown(TaxException)
+    }
+
+
+    def 'emptyReference'() {
+
+        when:
+        TaxInterface.cancelInvoice("   ")
+
+        then:
+        thrown(TaxException)
+    }
+
+
+    def 'referenceDoesNotExist'() {
+
+        when:
+        TaxInterface.cancelInvoice("XXXXXXXX")
+
+        then:
+        thrown(TaxException)
+    }
+
+}
