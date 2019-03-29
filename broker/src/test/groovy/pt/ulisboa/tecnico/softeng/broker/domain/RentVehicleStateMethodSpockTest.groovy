5e4f2b91d185f9b839745c81545d0984c44b6fa7
Fogoid <diogo.barradas.fernandes@gmail.com>
2019-03-29 10:34:47 +0000

    Changed the way broker constructor is called

--- broker/src/test/groovy/pt/ulisboa/tecnico/softeng/broker/domain/RentVehicleStateMethodSpockTest.groovy
+++ broker/src/test/groovy/pt/ulisboa/tecnico/softeng/broker/domain/RentVehicleStateMethodSpockTest.groovy
@@ -35,7 +35,7 @@ class RentVehicleStateMethodSpockTest extends SpockRollbackTestAbstractClass {
         //adventure = Mock(Adventure)
 
 
-        broker = new Broker("BR01", "eXtremeADVENTURE", BROKER_NIF_AS_SELLER, NIF_AS_BUYER, BROKER_IBAN, hotelInterface, carInterface)
+        broker = new Broker("BR01", "eXtremeADVENTURE", BROKER_NIF_AS_SELLER, NIF_AS_BUYER, BROKER_IBAN, null, null, carInterface, hotelInterface, null)
         client = new Client(broker, CLIENT_IBAN, CLIENT_NIF, DRIVING_LICENSE, AGE)
         adventure = new Adventure(broker, BEGIN, END, client, MARGIN)
 
