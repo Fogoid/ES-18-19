b18a2fb33df8b98125f0925d3b35244af45a7156
Fogoid <diogo.barradas.fernandes@gmail.com>
2019-03-29 10:14:02 +0000

    Changed constructor argument order

--- broker/src/test/groovy/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBookingGetRoomBookingData4TypeMethodSpockTest.groovy
+++ broker/src/test/groovy/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBookingGetRoomBookingData4TypeMethodSpockTest.groovy
@@ -29,7 +29,7 @@ class BulkRoomBookingGetRoomBookingData4TypeMethodSpockTest extends SpockRollbac
         hotelInterface = Mock(HotelInterface)
         carInterface = Mock(CarInterface)
 
-        broker = new Broker("BR01", "eXtremeADVENTURE", BROKER_NIF_AS_SELLER, NIF_AS_BUYER, BROKER_IBAN, hotelInterface, carInterface)
+        broker = new Broker("BR01", "eXtremeADVENTURE", BROKER_NIF_AS_SELLER, NIF_AS_BUYER, BROKER_IBAN, null, null, carInterface, hotelInterface, null)
         bulk = new BulkRoomBooking(broker, NUMBER_OF_BULK, BEGIN, END, NIF_AS_BUYER, CLIENT_IBAN)
 
         new Reference(bulk, REF_ONE)
